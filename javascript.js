fa542cbce793bfb99ca7ca3b3bd8fe4155ea52f5
Chun Sun <chunsun978@gmail.com>
2022-08-01 11:18:43 -0400

    Console script

---
+++ javascript.js
@@ -0,0 +1,9 @@
+// Lo to he console
+console.log('Hi my name is Chun');
+console.log(12345);
+
+// Console error
+console.error('This is an error')
+
+// console a table
+console.table({a:1, b:2, c:3, d:4})
